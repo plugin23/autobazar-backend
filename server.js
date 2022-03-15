e353f5d5180bf1056bb0f5131c4c2d3d5629b375
plugin23 <xvalicek@stuba.sk>
2022-03-15 22:50:55 +0100

    cleaned up server.js

--- server.js
+++ server.js
@@ -6,20 +6,7 @@ require('dotenv').config()
 const uri = process.env.MONGODB_URI;
 //app.use(bodyParser.urlencoded({ extended: true }))
 
-/*
-app.listen(8080, function() {})
-
-
-app.get('/', function(req, res) {
-    res.send('Hello World')
-  })
-
-app.post('/quotes', (req, res) => {
-  console.log('Hellooooooooooooooooo!')
-})  
-*/
-
-/*mongoose.connect(uri, {useNewUrlParser:true})
+mongoose.connect(uri, {useNewUrlParser:true})
 const db = mongoose.connection
 db.once('open', () => console.log('Connected to DB'))
 
@@ -27,60 +14,7 @@ const usersRouter = require('./routes/users')
 const carsRouter = require('./routes/cars')
 
 app.use('/api/autobazar/users', usersRouter)
-app.use('api/autobazar/cars', carsRouter)
-
-app.listen(8080, () => console.log('Server started'))*/
-
-const connectToDB = async () => {
-    await mongo().then(async (mongoose) => {
-        try{
-            console.log('Connectet to db')
-            //setCarSchema()
-            //setUserSchema()
-            
-        }
-        finally{
-            mongoose.connection.close()
-        }
-    })
-}
-
-connectToDB()
+app.use('/api/autobazar/cars', carsRouter)
 
 
-function setCarSchema(){
-    const user = {
-        id: 1,
-        first_name: 'Fero',
-        last_name: 'Mrkva',
-        phone_number: '0908777888',
-        favourites: [1,2,3,4],
-        password: 'hesielko',
-        created_at: "2021-01-01T12:00:00Z",
-    }
-
-    await new userSchema(user).save()
-}
-
-function setUserSchema(){
-    const car = {
-        id: 1,
-        author: 1,
-        year: 1999,
-        milage: 301555,
-        price: 5899,
-        doors: 4,
-        description: "test description",
-        engine_cap: "test",
-        car_brand: "test",
-        image_url: "test",
-        body: "test",
-        password: "test",
-        image_photos: [1,2,3],
-        created_at: "2021-01-01T12:00:00Z",
-    }
-
-    await new carSchema(car).save()
-}
-
-
+app.listen(8080, () => console.log('Server started'))
