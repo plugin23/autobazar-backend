5dec80b0641f4d38fd1f04097c14fcd0da04fe03
Daniel Cok <cok@asdata.sk>
2022-03-17 09:01:37 +0100

    added get user method

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,10 @@ const router = express.Router()
 const User = require('../schemas/user-schema')
 
 
-router.get('/', async (req, res) => {
+router.get('/:postId', async (req, res) => {
     try{
-        const users = await User.find()
+        const users = await User.find({_id: req.params.postId})
+        console.log(users);
         res.json(users)
     } catch(err) {
         res.status(500).json({message: err.message})
--- server.js
+++ server.js
@@ -9,8 +9,11 @@ mongoose.connect(process.env.MONGODB_URI, () => console.log("connected to db"))
 
 const getCars = require('./routes/cars');
 const deleteCars = require('./routes/cars');
+const getUserInfo = require('./routes/users');
+
 
 app.use('/api/autobazar/cars',getCars);
 app.use('/api/autobazar/cars',deleteCars);
+app.use('/api/autobazar/users',getUserInfo);
 
 app.listen(8080)
