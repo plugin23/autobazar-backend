bf3c44a836c7c2f9bd64452ccaefcaf7769434d2
plugin23 <xvalicek@stuba.sk>
2022-03-29 22:29:24 +0200

    dotenv change

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 import express from 'express'
 import mongoose from 'mongoose'
-import dotenv from 'dotenv'
 import {router as carsRouter} from '/skola/autobazar-backend/routes/cars.js'
 import {router as usersRouter} from '/skola/autobazar-backend/routes/users.js'
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
 
-//const firebase = require('firebase')
 const app = express()
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json({limit: '15mb'}));
 
 if (process.env.NODE_ENV !== 'production') {
-    dotenv.config()
+    require('dotenv').config()
 }
 
 mongoose.connect(process.env.MONGODB_URI, () => console.log("connected to db"))
