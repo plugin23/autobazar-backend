c498471851106e22711c95f84adac7833a1177fb
plugin23 <xvalicek@stuba.sk>
2022-04-08 13:26:50 +0200

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose'
 import {router as carsRouter} from './routes/cars.js'
 import {router as usersRouter} from './routes/users.js'
+import cors from 'cors'
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
@@ -9,15 +10,13 @@ const app = express()
 const PORT = process.env.PORT || 8080;
 
 app.use(express.json({limit: '15mb'}));
+app.use(cors())
 
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
 mongoose.connect(process.env.MONGODB_URI, () => console.log("connected to db"))
-
-app.use(express.json())
-
 
 app.use('/api/autobazar/cars', carsRouter);
 app.use('/api/autobazar/users', usersRouter);
