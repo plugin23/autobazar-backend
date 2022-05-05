3a3d6ab55f2e386f01d66a79e44a2ce7bcec020d
Daniel Cok <cok@asdata.sk>
2022-05-05 21:30:41 +0200

    added web socket

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ import {router as carsRouter} from './routes/cars.js'
 import {router as usersRouter} from './routes/users.js'
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
-
 const app = express()
+
+const http = require("http");
+const WebSocket = require("ws");
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
 const PORT = process.env.PORT || 8080;
+
+wss.on("connection", function connection(ws) {
+    ws.on("message", function incoming(message, isBinary) {
+      console.log(message.toString(), isBinary);
+      wss.client(function client() {
+        if (client.readyState === WebSocket.OPEN) {
+          client.send(message.toString());
+        }
+      });
+    });
+  });
 
 app.use(express.json({limit: '15mb'}));
 app.use(cors())
