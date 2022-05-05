7d9d3ddae560e4679ce10d55dd607bc35f58201d
Daniel Cok <cok@asdata.sk>
2022-05-05 21:58:47 +0200

    changed web socket

--- server.js
+++ server.js
@@ -6,24 +6,8 @@ import {router as usersRouter} from './routes/users.js'
 import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 const app = express()
-
-const http = require("http");
-const WebSocket = require("ws");
-const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
-
+const ws = require('ws');
 const PORT = process.env.PORT || 8080;
-
-wss.on("connection", function connection(ws) {
-    ws.on("message", function incoming(message, isBinary) {
-      console.log(message.toString(), isBinary);
-      wss.client(function client() {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(message.toString());
-        }
-      });
-    });
-  });
 
 app.use(express.json({limit: '15mb'}));
 app.use(cors())
@@ -37,4 +21,16 @@ mongoose.connect(process.env.MONGODB_URI, () => console.log("connected to db"))
 app.use('/api/autobazar/cars', carsRouter);
 app.use('/api/autobazar/users', usersRouter);
 
-app.listen(PORT)
+//app.listen(PORT)
+
+const wsServer = new ws.Server({ noServer: true });
+wsServer.on('connection', socket => {
+  socket.on('message', message => console.log(message));
+});
+
+const server = app.listen(PORT);
+server.on('upgrade', (request, socket, head) => {
+  wsServer.handleUpgrade(request, socket, head => {
+    wsServer.emit('connection', socket, request);
+  });
+});
